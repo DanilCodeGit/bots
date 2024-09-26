c0fe05303033d8bd1e8ba7a2770f9ec89c7a0d81
danil <naumkindv@txix.ru>
2024-09-27 01:02:19 +0300

    Stop tracking conf.go files

--- final/conf.go
+++
@@ -1,5 +0,0 @@
-package main
-
-const (
-	finaltoken = "7385636289:AAF54ZXLNkImvDB_x2IgvTSbXJx0OC2ymSU"
-)
--- release/conf.go
+++
@@ -1,5 +0,0 @@
-package main
-
-const (
-	releaseBot = "7865373070:AAECmLL6jBxwHPQUbWfrE_GtePG9bhx92Bg"
-)
